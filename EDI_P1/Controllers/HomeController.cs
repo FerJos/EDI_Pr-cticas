781de230e27cf6a2f93f4f8e03776bb026868b46
FerJos <fj.alegria@hotmail.com>
2022-02-02 16:04:51 -0600

    Crear clase Persona, editar métodos del controlador

--- EDI_P1/Controllers/HomeController.cs
+++ EDI_P1/Controllers/HomeController.cs
@@ -7,15 +7,48 @@ namespace EDI_P1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        List<Persona> personaList;
+        Persona cliente1;
+        Persona cliente2;
+        Persona cliente3;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            cliente1 = new Persona
+            {
+                Name = "Pedro",
+                LastName = "Aucob",
+                Phone = 1234567890,
+                Description = "Actor",
+            };
+            cliente2 = new Persona
+            {
+                Name = "Juan",
+                LastName = "López",
+                Phone = 1234567890,
+                Description = "Pintor",
+            };
+            cliente3 = new Persona
+            {
+                Name = "Jane",
+                LastName = "Doe",
+                Phone = 1234567890,
+                Description = "Comediante",
+            };
+            personaList = new List<Persona>();
+            personaList.Add(cliente1);
+            personaList.Add(cliente2);
+            personaList.Add(cliente3);
         }
 
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
             return View();
+        }*/
+        public List<Persona> Index()
+        {
+            return personaList;
         }
 
         public IActionResult Privacy()
