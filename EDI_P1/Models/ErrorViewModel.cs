6ad1e5ad5d756c851e6d548c0c1dabeeb21d45f7
FerJos <fj.alegria@hotmail.com>
2022-02-17 21:44:31 -0600

    Remover archivos del proyecto en desuso

--- EDI_P1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EDI_P1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
