fcb94f838726cd713afa92bb30eba6f3fa6441a2
FerJos <fj.alegria@hotmail.com>
2022-02-17 21:47:57 -0600

    Agregar nuevo proyecto

---
+++ EDI_P0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EDI_P0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
