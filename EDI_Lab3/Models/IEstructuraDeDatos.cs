235626c75642a47241991e65ae2cc53508f1d26c
FerJos <fj.alegria@hotmail.com>
2022-03-02 00:18:49 -0600

    Creación de controllers, modelos y vistas.

---
+++ EDI_Lab3/Models/IEstructuraDeDatos.cs
@@ -0,0 +1,10 @@
+﻿namespace EDI_Lab3.Models
+{
+    public interface IEstructuraDeDatos<T>
+    {
+        void Insertar(T valor);
+        void Actualizar(int pos);
+        void Eliminar(int pos);
+        T Obtener(int pos);
+    }
+}
---
+++ EDI_Lab3/Models/Persona.cs
@@ -0,0 +1,8 @@
+﻿namespace EDI_Lab3.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
