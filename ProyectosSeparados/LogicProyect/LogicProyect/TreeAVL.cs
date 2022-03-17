68a621b5743d72d01396d44185d1b9b08c5ac10e
FerJos <fj.alegria@hotmail.com>
2022-03-16 20:15:01 -0600

    Crear proyecto para probar métodos de forma aislada.

---
+++ ProyectosSeparados/LogicProyect/LogicProyect/TreeAVL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProyect
+{
+    internal class TreeAVL<T> where T : IComparable
+    {
+        public int Height { get; set; }
+        public TreeAVL<T> SubTree_Right { get; set; }
+        public TreeAVL<T> SubTree_Left { get; set; }
+
+        //public T Value
+
+        public TreeAVL() { }
+
+        public TreeAVL(TreeAVL<T> right = null, TreeAVL<T> left = null)
+        {
+            SubTree_Right = right;
+            SubTree_Left = left;
+
+            /*Value = value,
+             * Nodo.Value = Value
+            */
+        }
+
+        public int GetHeight(TreeAVL<T> subTree)
+        {
+            if (subTree == null)
+                return -1;
+            else
+                return 0;   //return subTree.Height;
+        }
+
+        public void UpdateHeight(TreeAVL<T> subTree)
+        {
+            if (subTree != null)
+            {
+                if(subTree.SubTree_Right.Height > subTree.SubTree_Left.Height)
+                    Height = subTree.SubTree_Right.Height + 1;
+                else
+                    Height = subTree.SubTree_Left.Height + 1;
+            }
+        }
+    }
+}
