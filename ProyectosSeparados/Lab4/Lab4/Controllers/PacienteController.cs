412403f93f3914e9fdfbd4ac1b23c900ed2efc86
LUIS ALEJANDRO GIRON TELLO <tatotello210@gmail.com>
2022-03-30 21:59:10 -0600

    Claseandida

---
+++ ProyectosSeparados/Lab4/Lab4/Controllers/PacienteController.cs
@@ -0,0 +1,45 @@
+﻿using Lab4.Models;
+using LaCola;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab4.Controllers
+{
+    [Route("[controlador]")]
+    public class PacienteController : Controller
+    {
+        public static Prioridades<Paciente> LaCola;
+
+        private List<Paciente> Pac = new List<Paciente>()
+        {
+            new Paciente(){Nombre = "Raul", Apellido = "Porres", FechaDeNacimiento = "12/04/2002", EstaSiendoAtendido=false, Sexo="Macho", Edad =18, Espec="Neumologia", Ingreso="Camilla"},
+            new Paciente(){Nombre = "Sandra", Apellido = "Porres", FechaDeNacimiento = "12/04/2002", EstaSiendoAtendido=false, Sexo="Macho", Edad =30, Espec="Anato", Ingreso="Camilla"},
+            new Paciente(){Nombre = "Paul", Apellido = "Porres", FechaDeNacimiento = "12/04/2002", EstaSiendoAtendido=false, Sexo="Macho", Edad =10, Espec="Link", Ingreso="Camilla"}
+
+        };
+
+        public PacienteController()
+        {
+            LaCola = new Prioridades<Paciente>(25);
+        }
+        
+        public Prioridades<Paciente> Index()
+        {
+            int inicilzador = 0;
+            foreach(var PAct in Pac)
+            {
+                if(PAct.Edad >= 18  && PAct.Edad <= 49)
+                {
+                    inicilzador = inicilzador + 3;
+                }
+                if (PAct.Edad >= 6 && PAct.Edad <= 17)
+                {
+                    inicilzador = inicilzador + 5;
+                }
+                LaCola.Encolar(PAct, inicilzador);
+            }
+            
+            return LaCola;
+        }
+    }
+}
