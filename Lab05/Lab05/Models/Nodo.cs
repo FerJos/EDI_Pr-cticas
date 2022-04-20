929035deaeddaca0677a642ca20ee8734bf7c827
SAMER ARANKI MUNOZ <saranki@correo.url.edu.gt>
2022-04-20 16:55:21 -0600

    clase nodo generico arreglado

--- Lab05/Lab05/Models/Nodo.cs
+++ Lab05/Lab05/Models/Nodo.cs
@@ -1,6 +1,15 @@
 ﻿namespace Lab05.Models
 {
-    public class Nodo
+    public class Nodo<T>
     {
+        T MinValue;
+        T MaxValue;
+        Nodo<T> subIzq;
+        Nodo<T> subMid;
+        Nodo<T> subDer;
+        public Nodo(T value)
+        {
+            MinValue = value;
+        }
     }
 }
